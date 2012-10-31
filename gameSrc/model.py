5daff475fc2359ad6ae304de679a8cdb55b3d38b
Matt <nerdfencer@gmail.com>
2012-10-31 15:39:37 -0400

    documentation

--- gameSrc/model.py
+++ gameSrc/model.py
@@ -11,6 +11,17 @@ import collision
 class Model():
     combiner = 0
     def __init__(self, fname,scale=1):
+        """__init__
+        parameters:
+            self
+            fname: path to the model file
+            scale=1: optional scaling factor for the model
+        returns:
+            none
+        Description:
+            Loads a model from a file and makes it ready for instancing.
+            Also applies node optimization and batches the static models.
+        """
         if Model.combiner == 0:
             Model.combiner = NodePath(RigidBodyCombiner("rbc"))
             Model.combiner.reparentTo(render)
@@ -19,6 +30,17 @@ class Model():
         self.modelRoot.reparentTo(Model.combiner)
         #print self.modelRoot.ls()
     def createInstance(self,scale=1,pos=(0,0,0),hpr=(0,0,0),show=1):
+        """createInstance
+        parameters:
+            self
+            scale=1: optional scaling factor for this instance
+            pos=(0,0,0): optional position to place the instance at
+            show=1: optional flag to determine whether or not it should be visible
+        returns:
+            none
+        Description:
+            Creates an instance of the model with specified transforms
+        """
         nnode = Model.combiner.attachNewNode("instance")
         copy = self.modelRoot.instanceTo(nnode)
         copy = nnode
@@ -27,9 +49,20 @@ class Model():
         nnode.setHpr(hpr)
         nnode.setScale(scale)
         if (not show):
-            nnode.detachNode()
+            nnode.hide()
         return nnode
     def createCopy(self,scale=1,pos=(0,0,0),hpr=(0,0,0),show=1):
+        """createCopy
+        parameters:
+            self
+            scale=1: optional scaling factor for this instance
+            pos=(0,0,0): optional position to place the instance at
+            show=1: optional flag to determine whether or not it should be visible
+        returns:
+            none
+        Description:
+            Creates a copy of the model with specified transforms
+        """
         nnode = self.modelRoot.copyTo(Model.combiner)
         nnode.setPos(pos)
         nnode.setHpr(hpr)
